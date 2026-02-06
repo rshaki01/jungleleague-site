22579cf35a172987c41cbfda961fe5665b7f48f8
Rafat Shakil <rshaki01@gmail.com>
2026-02-06 00:22:59 -0500

    Updated navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react'
+import { useState } from 'react';
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <header>
+        <div className='bg-gradient-to-r from-black via-gray-900 to-black backdrop-blur-md border-b border-yellow-400/20 sticky top-0 z-40 shadow-2xl text-white'>Jungle League</div>
+    </header>
+  )
+}
+
