e7465e626c02a7da6ae5f8956d49ef64f7ea4e06
Rafat Shakil <rshaki01@gmail.com>
2026-02-07 21:02:41 -0500

    Updates to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,31 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 border-b bg-white">
-        <nav className="flex ">
-            <Link href="/" className="text-lg font-extrabold">
+        <nav className="flex mx-auto max-w-7xl items-center px-6 py-6 justify-between">
+            <Link href="/" className="text-lg">
                 Jungle League
             </Link>
+            
+            {/* Navigation Links */}
+            <ul className="hidden gap-8 md:flex">
+                <li>
+                    <Link href="" className="hover:underline">Schedule</Link>
+                </li>
+
+                <li>
+                    <Link href="" className="hover:underline">Scores</Link>
+                </li>
+
+                <li>
+                    <Link href="" className="hover:underline">Statistics</Link>
+                </li>
+
+                <li>
+                    <Link href="" className="hover:underline">Standings</Link>
+                </li>
+            </ul>
         </nav>
-        
+
     </header>
   )
 }
