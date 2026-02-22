6aac4aa8b057a3a83c30fb85f89e62b5aa1efd10
Rafat Shakil <rshaki01@gmail.com>
2026-02-22 17:41:52 -0500

    Implemented Team Modal to display team name and record

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         
         {/* Hero Section */}
         <section className="min-h-screen flex items-center flex-col justify-center px-4 max-w-6xl text-center">
-          <h1 className="text-7xl md:text-7xl lg:text-7xl font-display font-bold  mb-6">
+          <h1 className="text-6xl md:text-7xl lg:text-7xl font-display font-bold  mb-6">
             <span className="bg-gradient-to-r from-yellow-300 to-yellow-500 bg-clip-text text-transparent">Season 2: "The Next Chapter"</span>
           </h1>
           <span className="bg-gradient-to-r from-yellow-400 to-yellow-200 bg-clip-text text-transparent text-xl font-bold ">Do you got next? Check out the Jungle Experience below!</span>
@@ -25,7 +25,6 @@ export default function Home() {
           
         </section>
 
-
         {/*Experience Section */}
         <section className="grid grid-cols-1 gap-10 px-5 md:grid-cols-2 mb-20">
           {/*Competitive Experience Cards */}
@@ -34,45 +33,45 @@ export default function Home() {
             <h1 className="text-xl sm:text-2xl font-display font-bold text-white mb-1 flex flex-col justify-center items-center">
               <span className="text-shadow-lg">Competitive League</span>
               <BoltIcon className="my-5 h-10 w-10 text-yellow-300" />
-              <span className="bg-gradient-to-r from-yellow-400 to-yellow-100 bg-clip-text text-transparent text-lg font-bold text-center opacity-40">Intensity meets opportunity.</span>
+              <span className="bg-gradient-to-r from-yellow-400 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-40">Intensity meets opportunity.</span>
             </h1>
             {/*Key Dates */}
             <div className="flex flex-col gap-2">
-              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-lg font-bold text-center opacity-90">April 19th</span>
-              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90">Jungle League Opener</span>
+              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-90 ">April 19th</span>
+              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90 text-sm">Jungle League Opener</span>
 
-              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-lg font-bold text-center opacity-90">May 24th</span>
-              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90">Jungle All Star Game</span>
+              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-90 ">May 24th</span>
+              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90 text-sm">Jungle All Star Game</span>
 
-              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-lg font-bold text-center opacity-90">July 5th</span>
-              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90">Playoffs: Race to the thrones</span>
+              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-90 ">July 5th</span>
+              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90 text-sm">Playoffs: Race to the thrones</span>
 
-              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-lg font-bold text-center opacity-90">July 19th</span>
-              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90">Playoffs: Race to the thrones</span>
+              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-90 ">July 19th</span>
+              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90 text-sm">Playoffs: Race to the thrones</span>
             </div>       
           </div>
 
           {/*Recreational Experience Cards */}
           <div className="h-[520px] w-full bg-gradient-to-br from-yellow-600/90 to-yellow-200/90 backdrop-blur-xl rounded-2xl py-10 px-10  flex flex-col items-center justify-between hover:from-yellow-300 hover:to-yellow-500 transition-all duration-300 transform">
             {/*Card Header */}
-            <h1 className="text-2xl sm:text-2xl font-display font-bold text-white mb-1 flex flex-col justify-center items-center text-center">
-              <span className="text-shadow-lg text-2xl">Recreational League</span>
+            <h1 className="text-lg sm:text-2xl font-display font-bold text-white mb-1 flex flex-col justify-center items-center text-center">
+              <span className="text-shadow-lg">Recreational League</span>
               <CommunityIcon className="my-5 h-10 w-10 text-yellow-300" />
-              <span className="bg-gradient-to-r from-yellow-400 to-yellow-100 bg-clip-text text-transparent text-lg font-bold text-center opacity-40">Community meets competition.</span>
+              <span className="bg-gradient-to-r from-yellow-400 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-40">Community meets competition.</span>
             </h1>
             {/*Key Dates */}
             <div className="flex flex-col gap-2">
-              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-lg font-bold text-center opacity-90">April 19th</span>
-              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90">Jungle League Opener</span>
+              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-90">April 19th</span>
+              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90 text-sm">Jungle League Opener</span>
 
-              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-lg font-bold text-center opacity-90">May 24th</span>
-              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90">Jungle All Star Game</span>
+              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-90">May 24th</span>
+              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90 text-sm">Jungle All Star Game</span>
 
-              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-lg font-bold text-center opacity-90">July 5th</span>
-              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90">Playoffs: Race to the thrones</span>
+              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-90">July 5th</span>
+              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90 text-sm">Playoffs: Race to the thrones</span>
 
-              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-lg font-bold text-center opacity-90">July 19th</span>
-              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90">Playoffs: Race to the thrones</span>
+              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-90">July 19th</span>
+              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90 text-sm text-sm">Playoffs: Race to the thrones</span>
             </div>
             
             
--- src/components/ui/TeamModal.jsx
+++ src/components/ui/TeamModal.jsx
@@ -8,15 +8,18 @@ export default function TeamModal({open, onClose, teamID}) {
 
      if (!open) return null
 
-     const team = getPlayersByTeamId(teamID);
-     console.log (team);
+     const players = getPlayersByTeamId(teamID);
+     const team = getTeamById(teamID);
 
     return (
         <div className="fixed inset-0 bg-black/50 backdrop-blur-sm transition-opacity duration-500 flex items-center justify-center" onClick={onClose}>
           <div className="max-w-6xl w-full max-h-[90vh] overflow-y-auto bg-slate-900 m-8 p-8 rounded-xl" onClick={(e) => e.stopPropagation()}>
-            <div className="flex justify-between">
-              <h1 className="text-slate-50 text-xl font-bold pb-4">Player: {team[1].name}</h1>
-              <button className="text-slate-50 cursor-pointer" onClick={onClose}>x</button>
+            <div className="flex justify-between py-2 w-full bg-blue-500 hover:bg-blue-400 text-white font-bold  px-4 rounded">
+              <div className='flex flex-col'>
+                <h1 className="text-slate-50 text-xl font-bold text-shadow-lg">{team.name}</h1>
+                <span className='text-sm items-align '>({team.wins} - {team.losses})</span>
+              </div>
+              <button className="text-slate-50 cursor-pointer flex items-align " onClick=      {onClose}>x</button>
             </div>
             <div className='my-2 gap-2'>
               <form >
@@ -24,11 +27,10 @@ export default function TeamModal({open, onClose, teamID}) {
                 <input className="rounded-lg border border-gray-700 bg-transparent w-full p-2 text-slate-300 hover:bg-slate-800" type="text" 
                 />
                 <div className='grid grid-cols-1 md:grid-cols-2 my-2 gap-3'>
-                  
-                    
+
              
                 </div>
-                <input type="submit" className="mt-8 w-full bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" value="Add Expense"/>
+      
               </form>
             </div>
           </div>
