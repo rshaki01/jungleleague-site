7bb8095df5de547c737e643985d7351d7d3d128b
Rafat Shakil <rshaki01@gmail.com>
2026-03-08 16:58:39 -0400

    updated score card to display scores

--- src/components/ui/scores/ScoresTable.jsx
+++ src/components/ui/scores/ScoresTable.jsx
@@ -15,7 +15,7 @@ export default function ScoresTable({division, week, teamId}) {
  
         <p>{division} {week} {teamId} </p>
         {/* Table Body - dynamically display each row*/}
-        {games.map((game) => (<div key={game.id}  className="flex flex-col gap-3 p-5 mb-5 bg-gradient-to-br from-gray-800/50 to-gray-900/30 rounded-xl border border-yellow-400/20 overflow-hidden text-xs font-semibold hover:bg-gray-700/30 transition-all duration-200 cursor-pointer">
+        {games.map((game) => (<div key={game.id}  className="flex flex-col gap-3 w- p-5 mb-5 bg-gradient-to-br from-gray-800/50 to-gray-900/30 rounded-xl border border-yellow-400/20 overflow-hidden text-xs font-semibold hover:bg-gray-700/30 transition-all duration-200 cursor-pointer">
             
             <div className="flex gap-3 pb-2 border-b border-gray-100/30 text-xs mx-auto text-gray-400/80">
               <div className="">Week: {game.week}</div>
@@ -23,12 +23,12 @@ export default function ScoresTable({division, week, teamId}) {
               <div className="">Time: {game.time}</div>
             </div>
             
-            <div className="flex flex-col gap-3 mx-auto ">
-              <div className="text-xl pb-2 text-center">
+            <div className="flex flex-col gap-3 mx-auto w-5/6 justify-between text-base">
+              <div className="pb-2 flex justify-between">
                 <span>{getTeamById(game.homeTeamId).name}</span>
                 <span>{game.homeScore}</span>
               </div>
-              <div className="text-xl text-center">
+              <div className="flex justify-between">
                 <span>{getTeamById(game.awayTeamId).name}</span>
                 <span>{game.awayScore}</span>
               </div>
