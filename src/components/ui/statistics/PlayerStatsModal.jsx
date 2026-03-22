2b779a0dd3c84e7a2208f60161685da5698a1e56
Rafat Shakil <rshaki01@gmail.com>
2026-03-21 20:50:27 -0400

    Added Player Stats Table

--- src/components/ui/statistics/PlayerStatsModal.jsx
+++ src/components/ui/statistics/PlayerStatsModal.jsx
@@ -2,13 +2,13 @@
 import {useState} from 'react';
 
 
-export default function PlayerStatsModal({division}) {
+export default function PlayerStatsModal({player, setModal}) {
 
     
 
 
     return (
-        <div className="fixed inset-0 z-[100] bg-black/50 backdrop-blur-sm transition-opacity duration-500 flex items-center justify-center">
+        <div className="fixed inset-0 z-[100] bg-black/50 backdrop-blur-sm transition-opacity duration-500 flex items-center justify-center" onClick={() => setModal(false)}>
             yo
         </div>
       )
--- src/components/ui/statistics/StatisticsClient.jsx
+++ src/components/ui/statistics/StatisticsClient.jsx
@@ -11,17 +11,16 @@ export default function StatisticsClient({division}) {
 
     const handleSelectPlayer = (player) => {
     setSelectedPlayer(player);     // pass full player object
-    setModalOpen(true);            // open modal
+    setModal(true);            // open modal
     setSearchTerm("");             // optional: clears input (closes dropdown)
     };
 
     const [searchTerm, setSearchTerm] = useState("");
     const [selectedPlayer, setSelectedPlayer] = useState("");
-    const [isModalOpen, setModalOpen] = useState(false);
+    const [isModalOpen, setModal] = useState(false);
     const filteredResults = searchPlayers(division, searchTerm);
 
 
-    console.log(selectedPlayer);
 
      // bg-gradient-to-br from-blue-400/10 to-blue-600/10 border border-blue-400/20
 
@@ -30,7 +29,7 @@ export default function StatisticsClient({division}) {
             <StatisticsFilter searchTerm={searchTerm} setSearchTerm={setSearchTerm} resultedPlayers={filteredResults} onSelectPlayer={handleSelectPlayer}/>            
             <StatisticsTable division={division} />
             
-            {isModalOpen && <PlayerStatsModal />}
+            {isModalOpen && <PlayerStatsModal player={selectedPlayer} setModal={setModal}/>}
         </div>
       )
 
