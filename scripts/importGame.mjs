9a4c7da895a32734ddcd36aee5e7e364c242813b
Rafat Shakil <rshaki01@gmail.com>
2026-04-15 00:12:46 -0400

    Updated import game validation

--- scripts/importGame.mjs
+++ scripts/importGame.mjs
@@ -11,20 +11,75 @@ function validateCompletedGame(gameData) {
     const errors = [];
 
     // validate if gameData object is an object
-    if (
+    const isValidObject =
     gameData &&
     typeof gameData === "object" &&
-    !Array.isArray(gameData)
-    ) {
-        // valid object
-    } else {
-    errors.push("Game data must be a valid object");
+    !Array.isArray(gameData);
+
+    if (!isValidObject) {
+        errors.push("Game data must be a valid object");
     }
 
+    // validate if top level-fields exist
+    const requiredFields = [
+    "id",
+    "division",
+    "week",
+    "date",
+    "time",
+    "status",
+    "homeTeamId",
+    "awayTeamId",
+    "homeScore",
+    "awayScore",
+    "boxScore",
+    ];
 
+
+    for (const field of requiredFields) {
+        if (!(field in gameData)) {
+            errors.push(`Missing field: ${field}`);
+        }
+    }
+
+    // validate top-level field types
+
+    const expectedTypes = {
+    "id": "string",
+    "division": "string",
+    "week": "number",
+    "date": "string",
+    "time": "string",
+    "status": "string",
+    "homeTeamId": "string",
+    "awayTeamId": "string",
+    "homeScore": "number",
+    "awayScore": "number",
+    "boxScore": "object"
+    }
+
+    for (const field in expectedTypes ) {
+        const expectedType = expectedTypes[field];
+        const actualValue = gameData[field];
+        if (typeof actualValue !== expectedType){
+        errors.push(`${field} field is incorrect`);
+        }
+    }
+
+    // 4. Validate if top-level values make sense
+    if (gameData.status === "completed" && gameData.homeTeamId !== gameData.awayTeamId && gameData.week >= 1 && gameData.homeScore >= 0 && gameData.awayScore >= 0) {
+        // valid
+
+    } else {
+        errors.push(`One of the top-level values do not make sense`);
+    }
+
+    // 5. Validate the box score shape
 
     return {
         isValid: errors.length === 0,
         errors,
     }
 }
+
+validateCompletedGame(gameData);
