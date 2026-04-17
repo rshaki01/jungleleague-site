4cd1afd17f0193011318d337359beb06e5eb04ef
Rafat Shakil <rshaki01@gmail.com>
2026-04-16 20:03:48 -0400

    Updated team table in standings to show per avg stats conditionally based on GP

--- src/components/ui/TeamTable.jsx
+++ src/components/ui/TeamTable.jsx
@@ -15,37 +15,51 @@ export default function TeamTable({players}) {
             </div>
     
             {/* Table Body - dynamically display each row*/}
-            {players.map((player) => (
-            <div key={player.id} className="hidden sm:grid grid-cols-6 gap-4 p-3 text-xs md:text-sm font-semibold text-gray-300 border-b border-gray-500/30">
-                <div className="col-span-2">{player.name}</div>
-                <div className="col-span-1 text-center">{player.ppg.toFixed(1)}</div>
-                <div className="col-span-1 text-center">{player.rpg.toFixed(1)}</div>
-                <div className="col-span-1 text-center">{player.apg.toFixed(1)}</div>        
-                <div className="col-span-1 text-center">{player.gp}</div>
-            </div>)
-            )}
+            {players.map((player) => {
+                
+                // calculate per game stats and set to 0 if games played = 0
+                const ppg = player.gp > 0 ? (player.pts / player.gp).toFixed(1) : 0
+                const rpg = player.gp > 0 ? (player.reb / player.gp).toFixed(1) : 0
+                const apg = player.gp > 0 ? (player.ast / player.gp).toFixed(1) : 0
+
+                return (
+                <div key={player.id} className="hidden sm:grid grid-cols-6 gap-4 p-3 text-xs md:text-sm font-semibold text-gray-300 border-b border-gray-500/30">
+                    <div className="col-span-2">{player.name}</div>
+                    <div className="col-span-1 text-center">{ppg}</div>
+                    <div className="col-span-1 text-center">{rpg}</div>
+                    <div className="col-span-1 text-center">{apg}</div>        
+                    <div className="col-span-1 text-center">{player.gp}</div>
+                </div>)
+            })}
             
             {/* Mobile Table Body - dynamically display each row*/}
-            {players.map((player) => (
-            <div key={player.id} className="flex flex-col sm:hidden gap-1 p-3 text-xs md:text-sm font-semibold border-b border-gray-500/30">
-                <div className="col-span-2 flex gap-2">
-                    {player.name}
-                    <span className="text-yellow-300/80">#{player.number}</span>
-                </div>
-                <div className="col-span-1 text-center flex gap-2 text-green-400">PPG:
-                    <span className="text-gray-100 font-normal">{player.ppg.toFixed(1)}</span>
-                </div>
-                <div className="col-span-1 text-center flex gap-2 text-blue-400">RPG:
-                    <span className="text-gray-100 font-normal">{player.rpg.toFixed(1)}</span>
-                </div>
-                <div className="col-span-1 text-center flex gap-2 text-orange-400">APG:
-                    <span className="text-gray-100 font-normal">{player.apg.toFixed(1)}</span>
-                </div>
-                <div className="col-span-1 text-center flex gap-2 text-red-400">GP:
-                    <span className="text-gray-100 font-normal">{player.gp}</span>
-                </div>
-            </div>)
-            )}
+            {players.map((player) => { 
+
+                 // calculate per game stats and set to 0 if games played = 0
+                const ppg = player.gp > 0 ? (player.pts / player.gp).toFixed(1) : 0
+                const rpg = player.gp > 0 ? (player.reb / player.gp).toFixed(1) : 0
+                const apg = player.gp > 0 ? (player.ast / player.gp).toFixed(1) : 0
+                
+                return (
+                <div key={player.id} className="flex flex-col sm:hidden gap-1 p-3 text-xs md:text-sm font-semibold border-b border-gray-500/30">
+                    <div className="col-span-2 flex gap-2">
+                        {player.name}
+                        <span className="text-yellow-300/80">#{player.number}</span>
+                    </div>
+                    <div className="col-span-1 text-center flex gap-2 text-green-400">PPG:
+                        <span className="text-gray-100 font-normal">{ppg}</span>
+                    </div>
+                    <div className="col-span-1 text-center flex gap-2 text-blue-400">RPG:
+                        <span className="text-gray-100 font-normal">{rpg}</span>
+                    </div>
+                    <div className="col-span-1 text-center flex gap-2 text-orange-400">APG:
+                        <span className="text-gray-100 font-normal">{apg}</span>
+                    </div>
+                    <div className="col-span-1 text-center flex gap-2 text-red-400">GP:
+                        <span className="text-gray-100 font-normal">{player.gp}</span>
+                    </div>
+                </div>)
+            })}
                   
         </div>   
     )
