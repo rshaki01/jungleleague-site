1fca4b64659787549ac9322f5f031bc4f138ec21
Rafat Shakil <rshaki01@gmail.com>
2026-05-09 03:19:07 -0400

    Updated homepage and imported week 3 games

--- src/app/page.js
+++ src/app/page.js
@@ -18,46 +18,16 @@ export default function Home() {
           <h1 className="text-6xl md:text-7xl lg:text-7xl font-display font-bold  mb-6">
             <span className="bg-gradient-to-r from-yellow-300 to-yellow-500 bg-clip-text text-transparent">Season 2: "The Next Chapter"</span>
           </h1>
-          <span className="bg-gradient-to-r from-yellow-400 to-yellow-200 bg-clip-text text-transparent text-xl font-bold ">Do you got next? Check out the Jungle Experience below!</span>
+          <span className="bg-gradient-to-r from-yellow-400 to-yellow-200 bg-clip-text text-transparent text-xl font-bold ">Think you got next? Check out our Instagram below!</span>
 
           {/* Sign up button */}
-          <a className="mt-6 text-2xl space-x-2 px-6 py-3 bg-gradient-to-r from-yellow-400 to-yellow-600 text-black rounded-xl font-bold hover:from-yellow-300 hover:to-yellow-500 transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-yellow-400/30 " target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/thejunglerecleague/">Sign up</a>
+          <a className="mt-6 px-6 py-3 bg-gradient-to-r from-yellow-400 to-yellow-600 text-black rounded-xl hover:from-yellow-300 hover:to-yellow-500 transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-yellow-400/30" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/thejunglerecleague/">
+            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+            </svg>
+          </a>
           
-        </section>
-
-        {/*Experience Section */}
-        <section className="grid grid-cols-1 gap-10 px-5 md:grid-cols-1 mb-20">
-          
-          {/*Key Date Card */}
-          <div className="h-[520px] w-full bg-gradient-to-br from-yellow-600/90 to-yellow-200/90 backdrop-blur-xl rounded-2xl py-10 px-10  flex flex-col items-center justify-between hover:from-yellow-300 hover:to-yellow-500 transition-all duration-300 transform">
-            {/*Card Header */}
-            <h1 className="text-lg sm:text-2xl font-display font-bold text-white mb-1 flex flex-col justify-center items-center text-center">
-              <span className="text-shadow-lg">The Experience: Key Dates</span>
-              <CommunityIcon className="my-5 h-10 w-10 text-yellow-300" />
-              <span className="bg-gradient-to-r from-yellow-400 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-40">Community meets competition.</span>
-            </h1>
-            {/*Key Dates */}
-            <div className="flex flex-col gap-2">
-              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-90">April 19th</span>
-              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90 text-sm">Jungle League Opener</span>
-
-              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-90">May 24th</span>
-              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90 text-sm">Jungle All Star Game</span>
-
-              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-90">July 5th</span>
-              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90 text-sm">Playoffs: Race to the thrones</span>
-
-              <span className="bg-gradient-to-r from-yellow-300 to-yellow-100 bg-clip-text text-transparent text-md font-bold text-center opacity-90">July 19th</span>
-              <span className="bg-clip-text text-shadow-sm text-base font-bold text-center opacity-90 text-sm text-sm">Playoffs: Race to the thrones</span>
-            </div>
-          </div>
-        </section>
-  
-        
-
-        
-        
-        
+        </section>        
        
       </main>
 
